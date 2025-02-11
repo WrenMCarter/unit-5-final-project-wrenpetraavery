b28946b2634875634fa935fa3db4dcc57f41f331
AveryBlouin <avery.blouin@student.csd509j.net>
2025-02-11 14:02:20 -0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,73 +1,96 @@
-function setUp (enemyImage: Image) {
-    for (let value of tiles.getTilesByType(assets.tile`enemyTile`)) {
-        enemy = sprites.create(assets.image`enemy`, SpriteKind.Enemy)
-        tiles.placeOnTile(enemy, value)
-        tiles.setTileAt(value, assets.tile`transparency16`)
-    }
-    for (let value2 of tiles.getTilesByType(assets.tile`myTile0`)) {
-        collectible = sprites.create(assets.image`coin0`, SpriteKind.Food)
-        tiles.placeOnTile(collectible, value2)
-        tiles.setTileAt(value2, assets.tile`transparency16`)
-    }
-    for (let value3 of tiles.getTilesByType(assets.tile`playerTile`)) {
-        playerSprite = sprites.create(assets.image`player`, SpriteKind.Player)
-        tiles.placeOnTile(playerSprite, value3)
-        tiles.setTileAt(value3, assets.tile`transparency16`)
-        controller.moveSprite(playerSprite, 100, 0)
-        playerSprite.ay = gravity
-        playerSprite.setStayInScreen(true)
-        scene.cameraFollowSprite(playerSprite)
+let gravity = 600;
+let walkspeed = 100;
+let maxjumps = 2;
+let jumpheight = 48;
+
+function MakeEnemies(appearance: Image) {
+    for (let v of tiles.getTilesByType(assets.tile`enemyTile`)) {
+        let enemy = sprites.create(appearance, SpriteKind.Enemy)
+        tiles.placeOnTile(enemy, v)
+        tiles.setTileAt(v, assets.tile`transparency16`)
     }
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (currentjumps < jumpNumber) {
-        playerSprite.vy = jumpSpeed
-        currentjumps += 1
+function MakeCoins(appearance: Image) {
+    for (let v of tiles.getTilesByType(assets.tile`coinTile`)) {
+        collectible = sprites.create(appearance, SpriteKind.Food)
+        tiles.placeOnTile(collectible, v)
+        tiles.setTileAt(v, assets.tile`transparency16`)
     }
+}
+function InitializeMap() {
+    if (level == 1) {
+        tiles.setCurrentTilemap(tilemap`level1`)
+    } else if (level == 2) {
+        tiles.setCurrentTilemap(tilemap`level1`)
+    } else if (level == 3) {
+        tiles.setCurrentTilemap(tilemap`level1`)
+    }
+}
+
+function MakePlayer(appearance: Image) {
+    info.setLife(3)
+    playerSprite = sprites.create(appearance, SpriteKind.Player)
+    tiles.placeOnRandomTile(playerSprite, assets.image`playerTile`)
+
+    playerSprite.ay = gravity
+    playerSprite.setStayInScreen(true)
+    scene.cameraFollowSprite(playerSprite)
+    controller.moveSprite(playerSprite, walkspeed, 0)
+}
+
+function SetUp () {
+    InitializeMap()
+    MakePlayer(assets.image`player`)
+    MakeEnemies(assets.image`enemy`)
+    MakeCoins(assets.image`coin0`)
+}
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (currentjumps >= maxjumps) {
+        return
+    }
+    playerSprite.vy = 0 - Math.sqrt(2 * (gravity * jumpheight))
+    currentjumps += 1
 })
+
 scene.onOverlapTile(SpriteKind.Player, assets.tile`levelTile`, function (sprite, location) {
     level += 1
     startLevel()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     sprites.destroy(otherSprite, effects.confetti, 500)
-    info.changeScoreBy(1)
+    info.changeLifeBy(1)
 })
+
 function startLevel () {
     sprites.destroyAllSpritesOfKind(SpriteKind.Player)
     sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
     sprites.destroyAllSpritesOfKind(SpriteKind.Food)
-    if (level == 1) {
-        tiles.setCurrentTilemap(tilemap`level1`)
-        setUp(assets.image`enemy`)
-    } else if (level == 2) {
-        tiles.setCurrentTilemap(tilemap`level1`)
-        setUp(assets.image`enemy`)
-    } else if (level == 3) {
-        tiles.setCurrentTilemap(tilemap`level1`)
-        setUp(assets.image`enemy`)
-    } else {
+
+    if (level > 3) {
         game.gameOver(true)
+        return
     }
+
+    SetUp()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(otherSprite, effects.fire, 500)
-    info.changeScoreBy(-1)
+    info.changeLifeBy(-1)
 })
+
 let currentjumps = 0
 let playerSprite: Sprite = null
+let endTile: Sprite = null
 let collectible: Sprite = null
 let enemy: Sprite = null
 let level = 0
-let jumpNumber = 0
-let gravity = 0
-let jumpSpeed = 0
 let list: number[] = []
-jumpSpeed = -75
-gravity = 100
-jumpNumber = 2
+
 level = 1
+info.setLife(3)
 startLevel()
+
 game.onUpdate(function () {
     if (playerSprite.isHittingTile(CollisionDirection.Bottom)) {
         currentjumps = 0
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -7,11 +7,11 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -254,12 +254,12 @@ namespace myTiles {
             case "tile1":return tile1;
             case "enemyTile":
             case "tile2":return tile2;
-            case "myTile0":
-            case "tile4":return tile4;
             case "myTile1":
             case "tile5":return tile5;
             case "levelTile":
             case "tile3":return tile3;
+            case "coinTile":
+            case "tile4":return tile4;
         }
         return null;
     })
